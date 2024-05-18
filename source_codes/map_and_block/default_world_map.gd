7d22ef6b295436824227a157cf03b2b72e67d165
nocturne123 <961099356@qq.com>
2024-05-19 00:30:49 +0800

    map_test
    点击地图获得对应的六边形坐标

--- source_codes/map_and_block/default_world_map.gd
+++ source_codes/map_and_block/default_world_map.gd
@@ -1,5 +1,7 @@
 extends TileMap
 
+@onready var map_camera = $map_camera
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,5 +16,6 @@ func _input(event):
 	if event is InputEventMouse:
 		if event.is_pressed():
 			var global_clicked = event.position
-			var pos_clicked = local_to_map(to_local(global_clicked))
-			print(pos_clicked)
+			var camera_pos = map_camera.get_local_mouse_position()
+			var mapped_position = local_to_map(camera_pos)
+			print(mapped_position)
