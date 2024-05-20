6e240018afd747f57f59e31351e5c54c48086c2e
nocturne123 <961099356@qq.com>
2024-05-21 00:21:54 +0800

    card_update
    尝试了卡牌的模组，cardui和carduidata需要再理一遍

---
+++ source_codes/card_system/cards/play/base_play.gd
@@ -0,0 +1,5 @@
+class_name Baseplay extends CardUIData
+
+@export var type:String
+@export var identity:String
+@export var description:String
--- source_codes/map_and_block/default_world_map.gd
+++ source_codes/map_and_block/default_world_map.gd
@@ -3,9 +3,12 @@ extends TileMap
 @onready var map_camera = $map_camera
 
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#设置地图的缩放
+	map_camera.zoom = Vector2(2,2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +18,9 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouse:
 		if event.is_pressed():
-			var global_clicked = event.position
+			
+			#点击地图时，打印出地图的对应坐标
+			#var global_clicked = event.position
 			var camera_pos = map_camera.get_local_mouse_position()
 			var mapped_position = local_to_map(camera_pos)
 			print(mapped_position)
