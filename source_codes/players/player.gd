494986e6c13d257c69c2ad5d41c9d83aeffdd8b1
nocturne123 <961099356@qq.com>
2024-06-09 23:34:35 +0800

    hand_pile_update

--- source_codes/players/player.gd
+++ source_codes/players/player.gd
@@ -5,6 +5,11 @@ enum PlayerState {Wait, Draw, Play, Discard}
 enum LivingState {Alive,Fainted,Dead}
 
 signal hand_pile_updated
+#due to the seperate of hand pile and draw,discard pile
+#now func like _maybe_remove and set_card_pile
+#will conmunicate by signal
+signal _maybe_remove_from_hand_pile
+signal remove_from_any_dropzone
 
 @export_group("基础属性")
 @export var player_name:String
@@ -44,6 +49,80 @@ signal hand_pile_updated
 #玩家在上一轮的生命值，记录给沙漏使用
 @export var health_last_turn:int = max_health
 
+@export_group("hand_pile_setting")
+@export var hand_pile_position = Vector2(640, 480)
+@export var hand_enabled := true
+@export var hand_face_up := true
+@export var max_hand_size := 10 # if any more cards are added to the hand, they are immediately discarded
+@export var max_hand_spread := 400
+@export var card_ui_hover_distance := 60
+@export var drag_when_clicked := true
+
+## This works best as a 2-point linear rise from -X to +X
+@export var hand_rotation_curve : Curve
+## This works best as a 3-point ease in/out from 0 to X to 0
+@export var hand_vertical_curve : Curve
+
 #手牌
-var hand_pile := []
+var _hand_pile := []
+
+#copyed from card_pile_ui
+#TODO:add chinese
+func _set_hand_pile_target_positions():
+	for i in _hand_pile.size():
+		var card_ui = _hand_pile[i]
+		card_ui.move_to_front()
+		var hand_ratio = 0.5
+		if _hand_pile.size() > 1:
+			hand_ratio = float(i) / float(_hand_pile.size() - 1)
+		var target_pos = hand_pile_position
+		var card_spacing = max_hand_spread / (_hand_pile.size() + 1)
+		target_pos.x += (i + 1) * card_spacing - max_hand_spread / 2.0
+		if hand_vertical_curve:
+			target_pos.y -= hand_vertical_curve.sample(hand_ratio)
+		if hand_rotation_curve:
+			card_ui.rotation = deg_to_rad(hand_rotation_curve.sample(hand_ratio))
+		if hand_face_up:
+			card_ui.set_direction(Vector2.UP)
+		else:
+			card_ui.set_direction(Vector2.DOWN)
+		card_ui.target_position = target_pos
+		#TODO:rewrite discard logic
+		#now just leave it there
+	#while _hand_pile.size() > max_hand_size:
+	#	set_card_pile(_hand_pile[_hand_pile.size() - 1], Piles.discard_pile)
+	_reset_hand_pile_z_index()
+
+#copyed from card_pile_ui.gd
+#TODO:add chinese	
+func _reset_hand_pile_z_index():
+	for i in _hand_pile.size():
+		var card_ui = _hand_pile[i]
+		card_ui.z_index = 1000 + i
+		card_ui.move_to_front()
+		if card_ui.mouse_is_hovering:
+			card_ui.z_index = 2000 + i
+		if card_ui.is_clicked:
+			card_ui.z_index = 3000 + i
 	
+#copyed from card_pile_ui.gd
+#TODO:add chinese
+func _maybe_remove_card_from_hand_piles(card : CardUI):
+	if _hand_pile.find(card) != -1:
+		_hand_pile.erase(card)
+		emit_signal("hand_pile_updated")
+		emit_signal("_maybe_remove_card_from_hand_piles",card)
+		
+func set_card_pile(card : CardUI):
+	_maybe_remove_card_from_hand_piles(card)
+	_maybe_remove_card_from_any_dropzones(card)
+	
+	_hand_pile.push_back(card)
+	emit_signal("hand_pile_updated")
+	_set_hand_pile_target_positions()
+	
+#due to all dropzone is managed by battle,
+#hand pile will only emit a signal
+#to let battle node do it's job
+func _maybe_remove_card_from_any_dropzones(card:CardUI):
+	emit_signal("remove_from_any_dropzone",card)
